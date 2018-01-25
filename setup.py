72b6b15b1d6fc86a9d57ab5c23498d8c1b2da160
lzz <lzz5235@vip.qq.com>
2018-01-25 21:20:31 +0800

    fix py2exe failed 	*sys.path.append('....')
    Signed-off-by: lzz <lzz5235@vip.qq.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # coding:utf-8
+import sys
+import py2exe
 from distutils.core import setup
 
+sys.path.append('C:\Python27\Lib\site-packages\py2exe')
 #op={"py2exe": {"includes": ["sip"],"dll_excludes":["MSVCP90.dll","MSVCR90.dll"]}}
 
 op={"py2exe": {'optimize': 0,'includes': ['sip','jinja2'],'packages':['ctypes','_ctypes','encodings'],'dll_excludes':["MSVCP90.dll","MSVCR90.dll"]}}
 setup(windows=[{"script":"ComponentDesignTool.py"}],options=op)
 
-#,"icon_resources":[(1,"E:/ISCAS/ProjectWizard/cdt.png")]}
 #python setup.py py2exe
